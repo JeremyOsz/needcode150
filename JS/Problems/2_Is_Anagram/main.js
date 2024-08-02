754bc0f940429e11fa0489c1a847354cb54c97cf
jeremy.osztreicher <jeremy.osztreicher@roh.org.uk>
2024-08-02 16:36:29 +0100

    Solve 2 in JS

--- JS/Problems/2_Is_Anagram/main.js
+++ JS/Problems/2_Is_Anagram/main.js
@@ -1,1 +1,48 @@
-module.exports = "Hello, Is_Anagram!";
+class Solution {
+  constructor() {
+    // Initialize any properties if needed
+  }
+
+  /**
+   * @param {string} s
+   * @param {string} t
+   * @return {boolean}
+   */
+  isAnagram(s, t) {
+    const sArr = s.split("").sort().join("");
+    const tArr = t.split("").sort().join("");
+    return sArr === tArr;
+  }
+
+  // For long strings or large inputs, a Frequency Counter is more efficient
+  /**
+   * @param {string} s
+   * @param {string} t
+   * @return {boolean}
+   */
+  isAnagramByFrequency(s, t) {
+    if (s.length !== t.length) {
+      return false;
+    }
+
+    const count = {};
+
+    for (let char of s) {
+      // Add 1 to the count if it exists, otherwise set it to 1
+      count[char] = (count[char] || 0) + 1;
+    }
+
+    for (let char of t) {
+      
+      // If single character is not in second string, it cannot be an anagram
+      if (!count[char]) {
+        return false;
+      }
+      count[char]--;
+    }
+
+    return true;
+  }
+}
+
+module.exports = Solution;
--- JS/Problems/2_Is_Anagram/main.test.js
+++ JS/Problems/2_Is_Anagram/main.test.js
@@ -8,3 +8,25 @@ test('main function output', () => {
     const expected = "Hello, Is_Anagram!";
     expect(output).toBe(expected);
 });
+
+// Eg. 1
+// Input: s = racecar, t = carrace 
+// Output: true
+test('anagram should return true', () => {
+    const solution = new Solution();
+    const output = solution.isAnagram("racecar", "carrace")
+    console.log(output)
+    const expected = true;
+    expect(output).toBe(expected);
+});
+
+// Eg. 2
+// Input: s = "jar", t = "jam"
+// Output: false
+test('anagram should return false', () => {
+    const solution = new Solution();
+    const output = solution.isAnagram("jar", "jam")
+    console.log(output)
+    const expected = false;
+    expect(output).toBe(expected);
+});
