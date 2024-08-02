06c8f2f777402533982bf0004eeeee094f73b59f
jeremy.osztreicher <jeremy.osztreicher@roh.org.uk>
2024-08-02 17:23:33 +0100

    Clean Problem 2 tests

--- JS/Problems/2_Is_Anagram/main.test.js
+++ JS/Problems/2_Is_Anagram/main.test.js
@@ -1,14 +1,5 @@
 const Solution = require('./main'); // Adjust the path if necessary
 
-// import 
-
-test('main function output', () => {
-    const output = Solution
-    console.log(output)
-    const expected = "Hello, Is_Anagram!";
-    expect(output).toBe(expected);
-});
-
 // Eg. 1
 // Input: s = racecar, t = carrace 
 // Output: true
