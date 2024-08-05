612b5010108808a4b0c716df583ea48c13203166
jeremy.osztreicher <jeremy.osztreicher@roh.org.uk>
2024-08-05 10:16:08 +0100

    Implement TwoSum function in Go

--- Go/Problems/3_Two_Integer_Sum/main.go
+++ Go/Problems/3_Two_Integer_Sum/main.go
@@ -7,7 +7,23 @@ func main() {
 }
 
 func TwoSum(nums []int, target int) []int {
+	// Initialize a map to store the indices of the numbers
+	numMap := make(map[int]int)
 
-	// Return an empty array if no solution is found
+	// Iterate through the slice
+	for i, num := range nums {
+		complement := target - num
+
+		// Check if the complement is already in the map
+		if index, found := numMap[complement]; found {
+			return []int{index, i}
+		}
+
+		// If the complement is not found, add the current number to the map
+		numMap[num] = i
+		fmt.Println(numMap)
+	}
+
+	// Return an empty slice if no solution is found
 	return []int{}
 }
